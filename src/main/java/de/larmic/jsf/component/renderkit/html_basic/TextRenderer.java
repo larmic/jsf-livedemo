9a873aabc45caeb1d0d0f4de5adce2d92c093f1e
Lars Michaelis <mail@larmic.de>
2016-01-21 17:13:30 +0100

    livedemo: extend component with surrounding div and style class

--- src/main/java/de/larmic/jsf/component/renderkit/html_basic/TextRenderer.java
+++ src/main/java/de/larmic/jsf/component/renderkit/html_basic/TextRenderer.java
@@ -2,9 +2,29 @@ package de.larmic.jsf.component.renderkit.html_basic;
 
 import de.larmic.jsf.component.html.UIText;
 
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
 import javax.faces.render.FacesRenderer;
 import javax.faces.render.Renderer;
+import java.io.IOException;
 
 @FacesRenderer(componentFamily = UIText.COMPONENT_FAMILY, rendererType = UIText.RENDERER_TYPE)
 public class TextRenderer extends Renderer {
+
+    @Override
+    public void encodeBegin(FacesContext context, UIComponent component) throws IOException {
+        final ResponseWriter writer = context.getResponseWriter();
+
+        writer.startElement("div", component);
+        writer.writeAttribute("style", "text-input", "styleClass");
+        writer.writeText("my first component", component, null);
+    }
+
+    @Override
+    public void encodeEnd(FacesContext context, UIComponent component) throws IOException {
+        final ResponseWriter writer = context.getResponseWriter();
+
+        writer.endElement("div");
+    }
 }
